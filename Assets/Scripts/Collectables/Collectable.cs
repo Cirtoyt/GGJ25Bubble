88495a94cf0c0b08d2b8c24d15b6df1fe8918ce0
Conrad Wilson <conradjwilson@hotmail.com>
2025-01-26 16:38:32 +0000

    Added Egg and win condition!!

--- Assets/Scripts/Collectables/Collectable.cs
+++ Assets/Scripts/Collectables/Collectable.cs
@@ -2,24 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Collectable : MonoBehaviour
+public abstract class Collectable : MonoBehaviour
 {
-    [SerializeField] GameObject pickup;
-
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnTriggerEnter(Collider other)
     {
-        //add to pickup count
+        if (other.GetComponent<PlayerController>() == null)
+            return;
+
+        OnPickup();
+
+        // Do pop particles
+
         Destroy(gameObject);
     }
+
+    protected abstract void OnPickup();
 }
