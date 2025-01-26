a595145feda9669427041f2dc254d397cfc05c0a
Conrad Wilson <conradjwilson@hotmail.com>
2025-01-26 13:00:23 +0000

    +Added Space Junk spawner system & added to Spaceship prefab

--- Assets/Scripts/Spaceship/EnemySpawner.cs
+++ Assets/Scripts/Spaceship/EnemySpawner.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private EnemyController _enemyToSpawn;
+    [Min(0)]
     [SerializeField] private int _minEnemySpawnVolume = 1;
+    [Min(0)]
     [SerializeField] private int _maxEnemySpawnVolume = 5;
+    [Min(0)]
     [SerializeField] private float _minEnemySpawnCooldown = 5;
+    [Min(0)]
     [SerializeField] private float _maxEnemySpawnCooldown = 20;
+    [Min(0)]
     [SerializeField] private float _enemySpawnCooldownOffsetRandomness = 2;
 
     private float spawnCooldownTimer = 0;
@@ -29,7 +34,6 @@ public class EnemySpawner : MonoBehaviour
 
             // Spawn enemies based on difficulty, adding posistive bias to combat flooring
             float posBias = (1f / (float)(_maxEnemySpawnVolume + 1)) * 0.5f;
-            Debug.Log(posBias);
             int spawnAmount = _minEnemySpawnVolume + Mathf.FloorToInt((_maxEnemySpawnVolume - _minEnemySpawnVolume) * (difficulty + posBias));
             for (int i = 0; i < spawnAmount; i++)
             {
@@ -39,7 +43,7 @@ public class EnemySpawner : MonoBehaviour
             // Set a new delay based on current difficulty, with a bit of randomness
             spawnCooldownTimer = (_maxEnemySpawnCooldown - ((_maxEnemySpawnCooldown - _minEnemySpawnCooldown) * difficulty)) + Random.Range(-_enemySpawnCooldownOffsetRandomness, _enemySpawnCooldownOffsetRandomness);
 
-            Debug.Log($"Spawned {spawnAmount} enemies! difficulty: {difficulty}, new spawnCooldownTimer = {spawnCooldownTimer}");
+            //Debug.Log($"Spawned {spawnAmount} enemies! difficulty: {difficulty}, new spawnCooldownTimer = {spawnCooldownTimer}");
         }
     }
 }
