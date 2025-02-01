a0d5bc478150ce64b9e7a11703a0a9d241b2ce31
Conrad Wilson <conrad@thepolygonloft.com>
2025-02-01 22:26:18 +0000

    +Added a indicator that goes around the edge of the screen to point towards the spaceship (the goal of the game) to make things less disorienting for players who get lost easily.

--- Assets/Scripts/UI/UI.cs
+++ Assets/Scripts/UI/UI.cs
@@ -6,6 +6,14 @@ using UnityEngine;
 
 public class UI : MonoSingleton<UI>
 {
+    [Header("Off-screen Indicator Variables")]
+    [SerializeField] Canvas _parentCanvas;
+    [Range(0f, 1f)]
+    [SerializeField] private float _indicatorBoundsMultiplier = 0.9f;
+    //[SerializeField] private float _indicatorBoundsOffset = 10f;
+    [SerializeField] private RectTransform _spaceshipIndicatorImage;
+    [SerializeField] private RectTransform _spaceshipIndicatorIconImage;
+
     [Header("Health Variables")]
     public int Health;
     [SerializeField] int NumHearts;
@@ -48,12 +56,149 @@ public class UI : MonoSingleton<UI>
 
     void Update()
     {
+        SetTargetPointersUI();
+
         SetTimer();
         SetTimerUIFill();
 
         SetHealthUI();
 
         UpdatePowerupUI();
+    }
+
+    private void SetTargetPointersUI()
+    {
+        // Collect positions
+        Vector3 spaceshipScreenPosition = PlayerController.Instance.Cam.WorldToScreenPoint(Spaceship.Instance.transform.position);
+        Debug.Log($"spaceshipScreenPosition: {spaceshipScreenPosition}");
+
+        //EnemyController[] enemies = FindObjectsByType<EnemyController>(FindObjectsSortMode.None);
+        //List<Vector3> enemyScreenPositions = new List<Vector3>();
+        //foreach(EnemyController enemy in enemies)
+        //{
+        //    enemyScreenPositions.Add(PlayerController.Instance.Cam.WorldToScreenPoint(enemy.transform.position));
+        //}
+
+        // Detect on/off screen
+        if (CheckObjectIsOnscreen(spaceshipScreenPosition))
+        {
+            // On-screen, hide indicator
+            _spaceshipIndicatorImage.gameObject.SetActive(false);
+        }
+        else
+        {
+            // Off-screen, show pointing indicator
+            _spaceshipIndicatorImage.gameObject.SetActive(true);
+
+            CalculateOffScreenIndicatorPosition(spaceshipScreenPosition, out Vector3 indicatorScreenPosition, out float indicatorAngle);
+
+            _spaceshipIndicatorImage.localPosition = indicatorScreenPosition;
+            _spaceshipIndicatorImage.localRotation = Quaternion.Euler(0, 0, indicatorAngle);
+            _spaceshipIndicatorIconImage.localRotation = Quaternion.Euler(0, 0, -indicatorAngle);
+        }
+    }
+
+    private bool CheckObjectIsOnscreen(Vector3 objectScreenPosition)
+    {
+        return objectScreenPosition.z > 0
+            && objectScreenPosition.x > 0 && objectScreenPosition.x <= Screen.width
+            && objectScreenPosition.y > 0 && objectScreenPosition.y <= Screen.height;
+    }
+
+    private void CalculateOffScreenIndicatorPosition(Vector3 objectScreenPosition, out Vector3 indicatorScreenPosition, out float indicatorDegAngle)
+    {
+        // Check to mirror screen position when spaceship is behind the camera
+        if (objectScreenPosition.z < 0)
+        {
+            //objectScreenPosition = -objectScreenPosition;
+            objectScreenPosition *= -1;
+            //objectScreenPosition.x = Screen.width - objectScreenPosition.x;
+            //objectScreenPosition.y = Screen.height - objectScreenPosition.y;
+        }
+
+        Vector3 screenCentre = new Vector3(Screen.width, Screen.height) * 0.5f;
+
+        // Temp shift position to 0,0 so can be used with atan2
+        objectScreenPosition -= screenCentre;
+
+        float angle = Mathf.Atan2(objectScreenPosition.y, objectScreenPosition.x);
+        angle -= 90 * Mathf.Deg2Rad;
+
+        // Return the angle
+        indicatorDegAngle = angle * Mathf.Rad2Deg;
+
+        // Get gradiant
+        float cos = Mathf.Cos(angle);
+        float sin = -Mathf.Sin(angle);
+
+        // y = mx + b format
+        float m = cos / sin;
+
+        // Lock image to bounds based on angle from 0, 0 (the shifted centre of screen position)
+        Vector3 screenBounds = (screenCentre * _indicatorBoundsMultiplier) / _parentCanvas.scaleFactor;
+        Debug.Log($"screenBounds: {screenBounds}");
+
+        // First check up & down
+        // x = y/m
+        if (cos > 0) // Check up
+            objectScreenPosition = new Vector3(screenBounds.y / m, screenBounds.y, 0);
+        else // Check down
+            objectScreenPosition = new Vector3(-screenBounds.y / m, -screenBounds.y, 0);
+
+        // If out of bounds along the x axis, get point on appropriate side of the screen
+        // y = m * x
+        if (objectScreenPosition.x > screenBounds.x) // Check right
+            objectScreenPosition = new Vector3(screenBounds.x, screenBounds.x * m);
+        else if (objectScreenPosition.x < -screenBounds.x) // Check left
+            objectScreenPosition = new Vector3(-screenBounds.x, -screenBounds.x * m);
+
+        // Undo the position shift to 0,0, and return position to screen centre
+        //objectScreenPosition += screenCentre;
+
+        // Set final transformed indicator position
+        indicatorScreenPosition = objectScreenPosition;
+    }
+
+    private void SetTimer()
+    {
+        if (timeRemaing < 0)
+        {
+            timeRemaing = 0;
+        }
+        if (timeRemaing > max)
+        {
+            timeRemaing = max;
+        }
+        if (timeRemaing > 0)
+        {
+            timeRemaing -= Time.deltaTime;
+        }
+        if (timeRemaing < endChaseTime)
+        {
+            timerText.color = Color.red;
+        }
+        int mins = Mathf.FloorToInt(timeRemaing / 60);
+        int secs = Mathf.FloorToInt(timeRemaing % 60);
+        timerText.text = timeRemaing.ToString();
+        timerText.text = string.Format("{0:00} : {1:00}", mins, secs);
+    }
+
+    private void SetTimerUIFill()
+    {
+        current = (int)timeRemaing;
+
+        float fillAmount = (float)current / (float)max;
+        mask.fillAmount = fillAmount;
+    }
+
+    public float GetTimerProgress(bool reverse = false)
+    {
+        float timerValueProgressAgainstMax = (float)timeRemaing / (float)max;
+
+        if (!reverse)
+            timerValueProgressAgainstMax = 1 - timerValueProgressAgainstMax;
+
+        return timerValueProgressAgainstMax;
     }
 
     void SetHealthUI()
@@ -78,48 +223,6 @@ public class UI : MonoSingleton<UI>
                 Hearts[i].enabled = false;
             }
         }
-    }
-
-    void SetTimer()
-    {
-        if (timeRemaing < 0)
-        {
-            timeRemaing = 0;
-        }
-        if (timeRemaing > max)
-        {
-            timeRemaing = max;
-        }
-        if (timeRemaing > 0)
-        {
-            timeRemaing -= Time.deltaTime;
-        }
-        if (timeRemaing < endChaseTime)
-        {
-            timerText.color = Color.red;
-        }
-        int mins = Mathf.FloorToInt(timeRemaing / 60);
-        int secs = Mathf.FloorToInt(timeRemaing % 60);
-        timerText.text = timeRemaing.ToString();
-        timerText.text = string.Format("{0:00} : {1:00}", mins, secs);
-    }
-
-    void SetTimerUIFill()
-    {
-        current = (int)timeRemaing;
-
-        float fillAmount = (float)current / (float)max;
-        mask.fillAmount = fillAmount;
-    }
-
-    public float GetTimerProgress(bool reverse = false)
-    {
-        float timerValueProgressAgainstMax = (float)timeRemaing / (float)max;
-
-        if (!reverse)
-            timerValueProgressAgainstMax = 1 - timerValueProgressAgainstMax;
-
-        return timerValueProgressAgainstMax;
     }
 
     public void Damage()
